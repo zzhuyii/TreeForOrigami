8195e31840fae365272cdf7393fc6bc96da1440f
Yi Zhu <37033716+zzhuyii@users.noreply.github.com>
2025-08-24 19:16:33 -0400

    Update MetamaterialSheet_ComputeRule_StreamlitApp.py

--- MetamaterialSheet_ComputeRule_StreamlitApp.py
+++ MetamaterialSheet_ComputeRule_StreamlitApp.py
@@ -42,6 +42,7 @@ def RunTreeDesign(BS30LB,BS30UB,BS60LB,BS60UB,BS90LB,BS90UB):
     
     print(np.shape(encodeX))
     st.text(np.shape(encodeX))
+    st.text(np.shape(otherX))
     #encodeFeatureName=encoder.get_feature_names()
     
     # Convert the names back to the normal names
