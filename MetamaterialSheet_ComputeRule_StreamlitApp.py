8a9c8563eb0d3620f2445974f838fe096eacd0af
Yi Zhu <37033716+zzhuyii@users.noreply.github.com>
2025-08-24 19:22:04 -0400

    Update MetamaterialSheet_ComputeRule_StreamlitApp.py

--- MetamaterialSheet_ComputeRule_StreamlitApp.py
+++ MetamaterialSheet_ComputeRule_StreamlitApp.py
@@ -40,6 +40,8 @@ def RunTreeDesign(BS30LB,BS30UB,BS60LB,BS60UB,BS90LB,BS90UB):
     encoder = OneHotEncoder()
     encodeX = encoder.fit_transform(categoryX)
     
+    encodeX=np.array(encodeX)
+    
     print(np.shape(encodeX))
     st.text(np.shape(encodeX))
     st.text(np.shape(otherX))
